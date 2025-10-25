8fe9824062a5271f42009d04c3f8d3cfcbd7d5d2
Elgun <elgunelgun800@gmail.com>
2025-10-25 21:41:09 +0400

    add chatbot

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,23 @@
-import './App.css'
-import { Header, Footer } from './shared/layout'
-import Router from './pages'
-import './i18n'
-import React, { useState } from 'react'
-import { ToastContainer } from 'react-toastify'
-import { SearchProvider } from './context/SearchContext'
+import "./App.css";
+import { Header, Footer } from "./shared/layout";
+import Router from "./pages";
+import "./i18n";
+import React, { useState } from "react";
+import { ToastContainer } from "react-toastify";
+import { SearchProvider } from "./context/SearchContext";
+import Chatbot from "./pages/Chatbot";
 function App() {
-
-  const [searchResults, setSearchResults] = useState([])
+  const [searchResults, setSearchResults] = useState([]);
 
   return (
     <SearchProvider>
       <Header onSearchResults={setSearchResults} />
       <Router searchResults={searchResults} />
       <Footer />
+      <Chatbot />
       <ToastContainer position="top-right" autoClose={3000} />
     </SearchProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,20 +1,17 @@
-import React from 'react'
-import HomeSlider from '../../shared/components/HomeSlider'
-import HeroSection from '../../shared/components/HeroSection'
-import StepsSection from '../../shared/components/StepsSection'
-import ExperienceSection from '@/shared/components/ExperienceSection'
-import ChatWidget from '@/shared/components/ChatWidget/chatWidget'
-const Home = ({searchResults }) => {
-
+import React from "react";
+import HomeSlider from "../../shared/components/HomeSlider";
+import HeroSection from "../../shared/components/HeroSection";
+import StepsSection from "../../shared/components/StepsSection";
+import ExperienceSection from "@/shared/components/ExperienceSection";
+const Home = ({ searchResults }) => {
   return (
     <div>
       <HomeSlider />
       <HeroSection />
       <StepsSection />
-      <ExperienceSection searchResults={searchResults }/>
-      <ChatWidget />
+      <ExperienceSection searchResults={searchResults} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
